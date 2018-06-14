20abd870fa78b1ac91dec0275e388aa45d73b8be
ravip18596 <ravip18596@gmail.com>
2018-06-14 22:13:44 +0530

    Files and Code Added

---
+++ load_facenet.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 14 18:21:09 2018
+
+@author: ravi
+"""
+
+import numpy as np
+import keras.models
+from keras.models import model_from_json
+import tensorflow as tf
+
+
+
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+
+    Returns:
+    loss -- real number, value of the loss
+    """
+
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+
+    ### START CODE HERE ### (≈ 4 lines)
+    # Step 1: Compute the (encoding) distance between the anchor and the positive
+    pos_dist =  tf.reduce_sum(tf.square(tf.subtract(anchor,positive)),axis=-1)
+    # Step 2: Compute the (encoding) distance between the anchor and the negative
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)),axis=-1)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = tf.add(tf.subtract(pos_dist,neg_dist),alpha)
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(basic_loss,0))
+    ### END CODE HERE ###
+
+    return loss
+
+def init(): 
+    json_file = open('facenet.json','r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    #load woeights into new model
+    loaded_model.load_weights("facenet_weights.h5")
+    print("Loaded Model from disk")
+    #compile and evaluate loaded model
+    loaded_model.compile(loss=triplet_loss,optimizer='adam',metrics=['accuracy'])
+    #loss,accuracy = model.evaluate(X_test,y_test)
+    #print('loss:', loss)
+    #print('accuracy:', accuracy)
+    graph = tf.get_default_graph()
+
+    return loaded_model,graph
---
+++ main2.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+import cv2
+import pickle
+import numpy as np
+import flask
+import io
+from PIL import Image
+from load_facenet import *
+
+app = flask.Flask(__name__)
+global model,graph
+model,graph = init()
+
+
+
+@app.route("/verify", methods=["POST"])            
+def verify():
+    name = flask.request.args.get("name")
+    image = flask.request.files["image"].read()
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    name = str(name)
+    image = Image.open(io.BytesIO(image))
+    img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face = faceCascade.detectMultiScale(gray,1.3,5)
+    output = {"code":-10,"message":"Start"}
+    if len(face)>0:
+        for (x,y,w,h) in face:
+            face = img[y:y+h,x:x+w]
+            resize_img = cv2.resize(face,(96,96))
+            with graph.as_default():
+                handle = open("encoding2.pickle","rb")
+                database = pickle.load(handle)
+                handle.close()
+                img = resize_img[...,::-1]
+                img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
+                x_train = np.array([img])
+                embedding = model.predict_on_batch(x_train)
+                try:
+                    dist = np.linalg.norm(embedding - database[name])
+                except:
+                    output["code"] = -3
+                    output["message"] = "Name does not exist in database"
+                    return flask.jsonify(output)
+                score = dist
+                print("Score:-  {}".format(score))
+                
+                handle = open("encoding2.pickle","wb")
+                pickle.dump(database,handle,protocol=pickle.HIGHEST_PROTOCOL)
+                handle.close()
+                if score <= 0.7:
+                    output["code"] = 1
+                    output["message"] = "Person is verified correctly"
+                    return flask.jsonify(output)
+                else:
+                    output["code"] = 0
+                    output["message"] = "Person is not verified"
+                    return flask.jsonify(output)
+                
+            
+            output["code"] = -2
+            output["message"] = "Model could not be loaded"
+            return flask.jsonify(output)
+    else:
+        output["code"] = -1
+        output["message"] = "No frontal face detected on picture"
+        return flask.jsonify(output)
+
+@app.route("/add", methods=["POST"])    
+def add():
+    name = flask.request.args.get("name")
+    image = flask.request.files["image"].read()
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    name = str(name)
+    image = Image.open(io.BytesIO(image))
+    img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face = faceCascade.detectMultiScale(gray,1.3,5)
+    output = {"code":-10,"message":"Inside add new face"}
+    if len(face)>0:
+        for (x,y,w,h) in face:
+            face = img[y:y+h,x:x+w]
+            resize_img = cv2.resize(face,(96,96))
+            handle = open("encoding2.pickle","rb")
+            database = pickle.load(handle)
+            handle.close()
+            with graph.as_default():
+                img = resize_img[...,::-1]
+                img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
+                x_train = np.array([img])
+                embedding = model.predict_on_batch(x_train)
+                database[name] = embedding
+                handle = open("encoding2.pickle","wb")
+                pickle.dump(database,handle,protocol=pickle.HIGHEST_PROTOCOL)
+                handle.close()
+                output["code"] = 1 
+                output["message"] = "Face added to the database"
+                return flask.jsonify(output)
+    else:
+        output["code"] = -1 
+        output["message"] = "No frontal face detected"
+        return flask.jsonify(output)
+        
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port="5000",debug=False)
+    
